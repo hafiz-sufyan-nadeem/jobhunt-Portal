b0a0a3256e0d44002bb9bd7460c2c09e2fa0abc5
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2026-06-11 16:47:13 +0500

    define middleware routes and make rolemiddleware file and register in app.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +17,18 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth', 'role:employer'])
+    ->prefix('employer')
+    ->name('employer.')
+    ->group(function (){
+        Route::get('/index', [EmployerController::class, 'index'])->name('index');
+        Route::get('/create', [EmployerController::class, 'create'])->name('create');
+        Route::post('/store', [EmployerController::class, 'store'])->name('store');
+        Route::get('/edit/{id}', [EmployerController::class, 'edit'])->name('edit');
+        Route::patch('/update/{id}', [EmployerController::class, 'update'])->name('update');
+    });
+
+
 
 require __DIR__.'/auth.php';
