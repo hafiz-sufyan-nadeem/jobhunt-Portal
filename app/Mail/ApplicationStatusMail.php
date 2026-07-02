ce5a1b837c6fe16e9509e455b33153b15293b1dd
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2026-07-02 18:24:36 +0500

    fix: ApplicationStatusMail class name PSR-4 compliance

--- app/Mail/ApplicationStatusMail.php
+++ app/Mail/ApplicationStatusMail.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class JobApplicationMail extends Mailable
+class ApplicationStatusMail extends Mailable
 {
     use Queueable, SerializesModels;
 
